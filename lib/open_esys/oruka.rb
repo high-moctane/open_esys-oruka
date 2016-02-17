59d3aaaaa7010b356d7989f1105b6e6a9c0491f6
high-moctane <sakaya_ppkds_yety-highoctane10492@yahoo.co.jp>
2016-02-17 15:25:28 +0900

    gem Parallel を使用することにした

--- lib/open_esys/oruka.rb
+++ lib/open_esys/oruka.rb
@@ -1,15 +1,22 @@
 require "open_esys/oruka/bluetooth"
 require "open_esys/oruka/version"
 
+require "parallel"
+
 module OpenEsys
   module Oruka
     #
+    # 最大同時接続数
+    MAX_CONNECTION = 7
+
+    #
     # 引数に与えられた btaddr からいるかどうかをスキャンする
-    # @param[Array[String]] list 存在を確認するリスト
-    # @return [Hash[String, Boolian
+    # @param[Hash[String, String]] list [名前, btaddr]
+    # @return [Hash[String, Boolian]] その名前がいるか
     def scan(list)
-      array = list.map { |item| [item, Bluetooth.exist?(item)] }
-      Hash[*array]
+      Parallel.map(list, in_threads: MAX_CONNECTION) {
+        |k, v| [k, Bluetooth.exist?(v)]
+      }.to_h
     end
   end
 end
--- lib/open_esys/oruka/version.rb
+++ lib/open_esys/oruka/version.rb
@@ -1,5 +1,5 @@
 module OpenEsys
   module Oruka
-    VERSION = "0.0.0.0"
+    VERSION = "0.0.0.1"
   end
 end
